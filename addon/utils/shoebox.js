7850c00e4b9fef162b376263580ea5fa07399dbe
Damian Senn <senn@puzzle.ch>
2019-03-29 11:30:51 +0100

    Update to new decorator spec

--- addon/utils/shoebox.js
+++ addon/utils/shoebox.js
@@ -1,30 +1,31 @@
 import { getOwner } from '@ember/application';
 import { get } from '@ember/object';
-import { resolve } from 'rsvp'
+import { resolve } from 'rsvp';
 
-export function shoeboxModelDecorator(target, key, descriptor) {
+export function shoeboxModelDecorator(f) {
   let initial = true;
-  let original = descriptor.value
-  descriptor.value = function shoeboxModel() {
-    if (!initial) return original.apply(this, arguments)
+  let original = f.descriptor.value;
 
-    let shoeboxName = `route-${this.routeName}__${key}`
-    let fastbootService = getOwner(this).lookup('service:fastboot')
-    let shoebox = get(fastbootService, 'shoebox')
+  f.descriptor.value = function shoeboxModel() {
+    if (!initial) return original.apply(this, arguments);
+
+    let shoeboxName = `route-${this.routeName}__${f.key}`;
+    let fastbootService = getOwner(this).lookup('service:fastboot');
+    let shoebox = get(fastbootService, 'shoebox');
     if (get(fastbootService, 'isFastBoot')) {
-      return resolve(original.apply(this, arguments))
-        .then(data => {
-          shoebox.put(shoeboxName, data)
-          return data
-        })
+      return resolve(original.apply(this, arguments)).then(data => {
+        shoebox.put(shoeboxName, data);
+        return data;
+      });
     }
 
-    initial = false
-    let data = shoebox.retrieve(shoeboxName)
+    initial = false;
+    let data = shoebox.retrieve(shoeboxName);
 
-    if (!data) return original.apply(this, arguments)
+    if (!data) return original.apply(this, arguments);
 
     return data;
-  }
-  return descriptor
+  };
+
+  return f;
 }
